88da6248c07aeff3eb17394238e518fc24ec7802
Divyanikadam <divyanikadam2002@gmail.com>
2024-12-30 11:26:50 +0530

    Added new files

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.querySelectorAll('input');
+input = [...input];
+
+
+
+ input.forEach(input,index) =>
+    {
+    input.addEventListener('keyup',(e) => 
+        {
+            if (e.key >= '0' && e.key <= '9') 
+            {
+                if (index + 1 < input.length)
+                     {
+                    input[index + 1].focus();
+                }
+            } 
+            else if (e.key === 'Backspace')
+             {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    };
